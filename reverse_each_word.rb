fd0069f760b7d25e512279d0ae97d3c224aa19ad
Milan Winter <milanwinter8@gmail.com>
2020-08-03 20:26:06 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+require 'pry'
+##def reverse_each_word string
+  ##array = string.split
+  ##array.collect{|element| element.reverse}.join(" ")
+##end
+def reverse_each_word string
+  string.split.collect{|element| element.reverse}.join(" ")
+end
